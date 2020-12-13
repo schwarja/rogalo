dfb61e003f0d8a491abcf31f04e32e7747b29375
Jan Schwarz <jan.schwarz@strv.com>
2020-12-13 23:08:55 +0100

    Enable bluetooth background mode Send a local notification on connection event

--- Rogalo/Common/Managers/Device/DeviceManager.swift
+++ Rogalo/Common/Managers/Device/DeviceManager.swift
@@ -6,11 +6,15 @@
 //
 
 import Combine
+import UIKit
+import UserNotifications
 
 class DeviceManager: DeviceManaging {
     let peripheral: Peripheral
     let bluetoothManager: BluetoothManaging
     let storage: Storage
+    
+    private var cancellables = Set<AnyCancellable>()
     
     var device: AnyPublisher<Device, Never> {
         bluetoothManager
@@ -23,6 +27,12 @@ class DeviceManager: DeviceManaging {
         self.peripheral = peripheral
         self.bluetoothManager = bluetoothManager
         self.storage = storage
+        
+        setup()
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (success, _) in
+            print("Notifications: \(success)")
+        }
     }
     
     func connect() {
@@ -37,4 +47,33 @@ class DeviceManager: DeviceManaging {
 
 // MARK: - Private methods
 private extension DeviceManager {
+    func content(for state: Device.State) -> UNMutableNotificationContent {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Connection status \(state)"
+        switch state {
+        case .connected:
+            content.sound = UNNotificationSound.default
+        case .connecting, .failed:
+            content.sound = UNNotificationSound.criticalSoundNamed(UNNotificationSoundName("Intel Chime.mp3"))
+        }
+        return content
+    }
+    
+    func sendNotification(for status: Device.State) {
+        let notification = content(for: status)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: notification, trigger: nil)
+
+        UNUserNotificationCenter.current().add(request)
+    }
+
+    func setup() {
+        device
+            .map(\.state)
+            .removeDuplicates()
+            .sink { [weak self] state in
+                self?.sendNotification(for: state)
+            }
+            .store(in: &cancellables)
+    }
 }
