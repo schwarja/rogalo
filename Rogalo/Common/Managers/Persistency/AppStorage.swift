33111bdf02ff8d9aa708306bc96c8fa9722dd966
Jan Schwarz <jan.schwarz@strv.com>
2021-03-07 17:39:18 +0100

    Persist revolutions multiplier

--- Rogalo/Common/Managers/Persistency/AppStorage.swift
+++ Rogalo/Common/Managers/Persistency/AppStorage.swift
@@ -9,8 +9,9 @@ import Combine
 import Foundation
 
 class AppStorage: Storage {
-    enum UserDefaultsKey: String {
+    enum UserDefaultsKey: String, CaseIterable {
         case peripheral
+        case revolutionsMultiplier
     }
     
     private let container: UserDefaults
@@ -19,6 +20,7 @@ class AppStorage: Storage {
     private var cancellables = Set<AnyCancellable>()
     
     let pairedDevice = CurrentValueSubject<Peripheral?, Never>(nil)
+    let revolutionsMultiplier = CurrentValueSubject<Float, Never>(1)
     
     init(container: UserDefaults) {
         self.container = container
@@ -27,35 +29,85 @@ class AppStorage: Storage {
     }
 }
 
-// MARK: Private methods
+// MARK: Setup
 private extension AppStorage {
     func setup() {
+        for key in UserDefaultsKey.allCases {
+            switch key {
+            case .peripheral:
+                setupEncodableObject(for: .peripheral, type: Peripheral.self, subject: pairedDevice)
+            case .revolutionsMultiplier:
+                setupFloat(for: .revolutionsMultiplier, subject: revolutionsMultiplier)
+            }
+        }
+    }
+    
+    func setupEncodableObject<T: Codable>(for key: UserDefaultsKey, type: T.Type, subject: CurrentValueSubject<T?, Never>) {
         if
-            let rawData = data(for: .peripheral),
-            let device = try? decoder.decode(Peripheral.self, from: rawData)
+            let rawData = data(for: key),
+            let object = try? decoder.decode(type, from: rawData)
         {
-            pairedDevice.value = device
+            subject.value = object
         }
         
-        pairedDevice
-            .map { [weak self] device in
+        subject
+            .map { [weak self] object in
                 do {
-                    return try self?.encoder.encode(device)
+                    return try self?.encoder.encode(object)
                 } catch {
                     return nil
                 }
             }
             .sink { [weak self] data in
-                self?.store(data, for: .peripheral)
+                self?.store(data, for: key)
             }
             .store(in: &cancellables)
+    }
+    
+    func setupFloat(for key: UserDefaultsKey, subject: CurrentValueSubject<Float, Never>) {
+        if let value = float(for: key) {
+            subject.value = value
+        }
+        
+        subject
+            .sink { [weak self] value in
+                self?.store(value, for: key)
+            }
+            .store(in: &cancellables)
+    }
+}
+ 
+// MARK: Getters/Setters
+private extension AppStorage {
+    func decode<T: Decodable>(for key: UserDefaultsKey) -> T? {
+        guard
+            let rawData = data(for: key),
+            let object = try? decoder.decode(T.self, from: rawData)
+        else
+        {
+            return nil
+        }
+        
+        return object
+    }
+    
+    func encode<T: Encodable>(_ object: T) -> Any? {
+        do {
+            return try encoder.encode(object)
+        } catch {
+            return nil
+        }
     }
     
     func data(for key: UserDefaultsKey) -> Data? {
         container.data(forKey: key.rawValue)
     }
     
-    func store(_ data: Data?, for key: UserDefaultsKey) {
-        container.set(data, forKey: key.rawValue)
+    func float(for key: UserDefaultsKey) -> Float? {
+        container.float(forKey: key.rawValue)
+    }
+    
+    func store(_ value: Any?, for key: UserDefaultsKey) {
+        container.set(value, forKey: key.rawValue)
     }
 }
